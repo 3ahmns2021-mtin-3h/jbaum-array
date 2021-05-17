252f1d47892f577a93d891d97158cbae21a45824
JanaBaumgartner <jbaumgartner0110@icloud.com>
2021-05-17 14:08:01 +0200

    exercise3

--- Assets/MyGame/Scripts/PracticeArrays.cs
+++ Assets/MyGame/Scripts/PracticeArrays.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PracticeArrays : MonoBehaviour
 
-{ public int[] array1 = new int[4];
+{
+    public int[] array1 = new int[4];
     public int[] array2 = new int[4] { 21, 22, 23, 24 };
     public int[] array3 = { 31, 32, 33, 34, 35, 36, 37 };
     public string[] array4 = new string[10];
@@ -13,9 +14,9 @@ public class PracticeArrays : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
 
-            for (int i = 0; i < array4.Length; i++)
+
+        for (int i = 0; i < array4.Length; i++)
         {
             array4[i] = "array4";
         }
@@ -35,11 +36,23 @@ public class PracticeArrays : MonoBehaviour
         Debug.Log("Element 0 von array4 hat den Wert:" + array4[array4.Length - 1]);
         Debug.Log("Name des ersten GameObjects: " + arrayColoredImagesRed[arrayColoredImagesRed.Length - 1].name);
 
+
+        PrintElements(array4);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    public void PrintElements(string[] x)
+    {
+        for (int i = 0; i < x.Length; i++)
+        {
+            Debug.Log("Element Nummer: " + i + "hat den Wert: " + x[i]);
+        }
+
     }
 }
